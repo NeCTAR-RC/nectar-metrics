bc13d5b89c0a40adecda37343d78b770b76597f9
Russell Sim <russell.sim@gmail.com>
2014-10-03 17:59:27 +1000

    Re-enable per-az reporting
    Change-Id: I646c9bc64853c5d868bf40993bb5ed134a41990d

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import nectar-metrics
+import nectar_metrics
 
 # -- General configuration ---------------------------------------------
 
