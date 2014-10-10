99d174abe15bb2f0a4bb4bfa381595640e004fb6
Russell Sim <russell.sim@gmail.com>
2014-10-10 13:36:28 +1100

    Enable logging
    This should mean less emails in the long run.  Basically if data goes
    missing it will continue to work, but will print errors.
    
    Change-Id: Ia97c45fe9b28cd29d30cad90d5c894f69dccd71f

---
+++ nectar_metrics/log.py
@@ -0,0 +1,40 @@
+from os import path
+import logging.config
+
+from nectar_metrics.config import CONFIG
+
+
+def setup(filename, file_level='INFO', console_level='INFO'):
+    config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'simple': {
+                'format': '%(asctime)s %(name)s %(levelname)s %(message)s'
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': console_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'file': {
+                'level': file_level,
+                'class': 'logging.FileHandler',
+                'formatter': 'simple',
+                'filename': "",
+            },
+        },
+        'root': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    }
+    log_dir = CONFIG.get('metrics', 'log_dir')
+    if log_dir:
+        config['handlers']['file']['filename'] = path.join(log_dir, filename)
+    else:
+        del config['handlers']['file']
+        config['root']['handlers'].remove('file')
+    logging.config.dictConfig(config)
