15647b4e67189579a01c4ce31932165cbb1208d1
Sam Morrison <sorrison@gmail.com>
2018-04-27 14:15:21 +1000

    Add pollsters for swift disks
    Change-Id: I6977aeceecbfc45b3864a362855ff68a6562792c

---
+++ nectar_metrics/ceilometer/objectstore/discovery.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+from ceilometer.agent import plugin_base
+
+
+class SwiftDiskDiscovery(plugin_base.DiscoveryBase):
+
+    def discover(self, manager, param=None):
+        """Discover object server disks."""
+
+        response = requests.get('http://127.0.0.1:6000/recon/diskusage')
+        return json.loads(response.text)
