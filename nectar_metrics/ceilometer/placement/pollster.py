c78e9338914f7fba6bf9ae35ab48bf31062799a2
Sam Morrison <sorrison@gmail.com>
2018-08-13 10:49:06 +1000

    Discovery and poller for placement resource providers
    Supports VCPU, MEMORY_MB and DISK_GB usages and capacities of
    placement resource providers
    
    Change-Id: Id476fc340d302a0aa5e317b965b6ad0a0744f463

---
+++ nectar_metrics/ceilometer/placement/pollster.py
@@ -0,0 +1,118 @@
+import itertools
+
+try:
+    # queens
+    from ceilometer.polling import plugin_base
+except ImportError:
+    # < queens
+    from ceilometer.agent import plugin_base
+from ceilometer import sample
+from ceilometer import keystone_client
+
+from placementclient import client
+
+
+class ResourceProviderPollster(plugin_base.PollsterBase):
+    """ Collect stats on resource providers
+    """
+
+    def __init__(self, conf):
+        super(ResourceProviderPollster, self).__init__(conf)
+        creds = conf.service_credentials
+        self.client = client.Client(
+            version='1',
+            session=keystone_client.get_session(conf),
+            region_name=creds.region_name,
+            interface=creds.interface,
+        )
+
+    @property
+    def default_discovery(self):
+        return 'resource_providers'
+
+    def _make_sample(self, metric, value, resource_provider, unit):
+        return sample.Sample(
+            name='resource_provider.%s' % metric,
+            type=sample.TYPE_GAUGE,
+            unit=unit,
+            volume=value,
+            user_id=None,
+            project_id=None,
+            resource_id=resource_provider.id,
+            resource_metadata={'name': resource_provider.name})
+
+    def _make_total_sample(self, metric, value, unit):
+        return sample.Sample(
+            name='global.resource_providers.%s' % metric,
+            type=sample.TYPE_GAUGE,
+            unit=unit,
+            volume=value,
+            user_id=None,
+            project_id=None,
+            resource_id='global-stats')
+
+    def get_samples(self, manager, cache, resources):
+        samples = []
+        vcpu_usage = 0
+        memory_usage = 0
+        disk_usage = 0
+        vcpu_capacity = 0
+        memory_capacity = 0
+        disk_capacity = 0
+
+        for resource_provider in resources:
+            usages = resource_provider.usages()
+            capacity = resource_provider.inventories()
+            if hasattr(usages, 'VCPU'):
+                samples.append(
+                    self._make_sample('usage.vcpu',
+                                      usages.VCPU,
+                                      resource_provider, 'VCPU'))
+                vcpu_usage += usages.VCPU
+            if hasattr(usages, 'MEMORY_MB'):
+                samples.append(
+                    self._make_sample('usage.memory',
+                                      usages.MEMORY_MB,
+                                      resource_provider, 'MB'))
+                memory_usage += usages.MEMORY_MB
+            if hasattr(usages, 'DISK_GB'):
+                samples.append(
+                    self._make_sample('usage.disk',
+                                      usages.DISK_GB,
+                                      resource_provider, 'GB'))
+                disk_usage += usages.DISK_GB
+            if hasattr(capacity, 'VCPU'):
+                samples.append(
+                    self._make_sample('capacity.vcpu',
+                                      capacity.VCPU['total'],
+                                      resource_provider, 'VCPU'))
+                vcpu_capacity += capacity.VCPU['total']
+            if hasattr(capacity, 'MEMORY_MB'):
+                samples.append(
+                    self._make_sample('capacity.memory',
+                                      capacity.MEMORY_MB['total'],
+                                      resource_provider, 'MB'))
+                memory_capacity += capacity.MEMORY_MB['total']
+            if hasattr(capacity, 'DISK_GB'):
+                samples.append(
+                    self._make_sample('capacity.disk',
+                                      capacity.DISK_GB['total'],
+                                      resource_provider, 'GB'))
+                disk_capacity += capacity.DISK_GB['total']
+
+        samples.append(
+            self._make_total_sample('usage.vcpu', vcpu_usage, 'VCPU'))
+        samples.append(
+            self._make_total_sample('usage.memory', memory_usage, 'MB'))
+        samples.append(
+            self._make_total_sample('usage.disk', disk_usage, 'GB'))
+        samples.append(
+            self._make_total_sample('capacity.vcpu', vcpu_capacity, 'VCPU'))
+        samples.append(
+            self._make_total_sample('capacity.memory', memory_capacity, 'MB'))
+        samples.append(
+            self._make_total_sample('capacity.disk', disk_capacity, 'GB'))
+
+        sample_iters = []
+        sample_iters.append(samples)
+        return itertools.chain(*sample_iters)
