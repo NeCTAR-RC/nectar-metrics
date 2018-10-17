5456164c9f628c4ae8728b25a5bef4058feefed0
Sam Morrison <sorrison@gmail.com>
2018-10-18 10:09:02 +1100

    Add network IP availabililty pollster
    Change-Id: I17f1625adad11bb8b2d45b5d95a1c363d72fc07a

---
+++ nectar_metrics/ceilometer/network/pollster.py
@@ -0,0 +1,45 @@
+import itertools
+
+from ceilometer.polling import plugin_base
+from ceilometer import sample
+
+from oslo_log import log
+LOG = log.getLogger(__name__)
+
+
+class NetworkIPAvailabilityPollster(plugin_base.PollsterBase):
+    """Collect stats on network IP availability"""
+
+    @property
+    def default_discovery(self):
+        return 'network_ip_availability'
+
+    def get_samples(self, manager, cache, resources):
+        samples = []
+
+        for network in resources:            
+            samples.append(sample.Sample(
+                name='ip.availability.used',
+                type=sample.TYPE_GAUGE,
+                unit=None,
+                volume=network['used_ips'],
+                user_id=None,
+                project_id=network['tenant_id'],
+                resource_id=network['network_id'],
+                resource_metadata={'name': network['network_name'])
+            )
+            samples.append(sample.Sample(
+                name='ip.availability.total',
+                type=sample.TYPE_GAUGE,
+                unit=None,
+                volume=network['total_ips'],
+                user_id=None,
+                project_id=network['tenant_id'],
+                resource_id=network['network_id'],
+                resource_metadata={'name': network['network_name'])
+            )
+
+        sample_iters = []
+        sample_iters.append(samples)
+        LOG.debug("Sending samples %s", sample_iters)
+        return itertools.chain(*sample_iters)
