679980d7c28148a2739fbee6f0a5f37797b8cddb
Paul van den Bergen <paul.vandenbergen@ardc.edu.au>
2019-06-18 17:10:47 +1000

    configure initial murano stats in ceilometer
    Change-Id: I85e34a1bae6aeea82f56ce4d68b9372d4d231df9

---
+++ nectar_metrics/ceilometer/application_catalog/pollster.py
@@ -0,0 +1,34 @@
+import itertools
+
+from ceilometer.polling import plugin_base
+from ceilometer import sample
+
+from oslo_log import log
+LOG = log.getLogger(__name__)
+
+
+class EnvironmentPollster(plugin_base.PollsterBase):
+    """Collect stats on murano application environments"""
+
+    @property
+    def default_discovery(self):
+        return 'application_catalog'
+
+    def get_samples(self, manager, cache, resources):
+        samples = []
+
+        total = sample.Sample(
+            name='global.application_catalog.environments',
+            type=sample.TYPE_GAUGE,
+            unit='environments',
+            volume=len(resources),
+            user_id=None,
+            project_id=None,
+            resource_id='global-stats')
+
+        samples.append(total)
+
+        sample_iters = []
+        sample_iters.append(samples)
+        LOG.debug("Sending samples %s", sample_iters)
+        return itertools.chain(*sample_iters)
