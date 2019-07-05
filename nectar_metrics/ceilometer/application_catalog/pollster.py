78819dec7a1f68a331af787a79cbcbc35e4aa5d5
Paul van den Bergen <paul.vandenbergen@ardc.edu.au>
2019-07-05 16:07:20 +1000

    expand environment stats for ceilometer/gnocchi
    Change-Id: Ie73187dbfc1995bad96a8455afa49c4ff7110bd4

--- nectar_metrics/ceilometer/application_catalog/pollster.py
+++ nectar_metrics/ceilometer/application_catalog/pollster.py
@@ -1,3 +1,4 @@
+import collections
 import itertools
 
 from ceilometer.polling import plugin_base
@@ -27,6 +28,22 @@ class EnvironmentPollster(plugin_base.PollsterBase):
             resource_id='global-stats')
 
         samples.append(total)
+
+        env_stats = collections.defaultdict(int)
+        for env in resources:
+            env_stats[env.status] += 1
+
+        for stats in env_stats:
+            s = sample.Sample(
+                name='global.application_catalog.environments',
+                type=sample.TYPE_GAUGE,
+                unit="environments.{}".format(stats),
+                volume=env_stats[stats],
+                user_id=None,
+                project_id=None,
+                resource_id='global-stats'
+                )
+            samples.append(s)
 
         sample_iters = []
         sample_iters.append(samples)
