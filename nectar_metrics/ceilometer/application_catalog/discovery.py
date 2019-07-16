d1bc2ef0eb15fbb7ee5a506cde71793fe597ec40
Andy Botting <andy@andybotting.com>
2019-07-16 16:05:17 +1000

    Add App Catalog package metrics
    Change-Id: I503360da99cc778e25384c5a9cd28f9827854bd5

--- nectar_metrics/ceilometer/application_catalog/discovery.py
+++ nectar_metrics/ceilometer/application_catalog/discovery.py
@@ -1,13 +1,14 @@
 from ceilometer import keystone_client
 from ceilometer.polling import plugin_base
 
+from collections import defaultdict
+
 from muranoclient import client
 
 
-class EnvironmentDiscovery(plugin_base.DiscoveryBase):
-
+class _BaseDiscovery(plugin_base.DiscoveryBase):
     def __init__(self, conf):
-        super(EnvironmentDiscovery, self).__init__(conf)
+        super(_BaseDiscovery, self).__init__(conf)
         creds = conf.service_credentials
         self.client = client.Client(
             version='1',
@@ -16,5 +17,16 @@ class EnvironmentDiscovery(plugin_base.DiscoveryBase):
             service_type='application-catalog',
         )
 
+
+class EnvironmentDiscovery(_BaseDiscovery):
     def discover(self, manager, param=None):
+        """Discover environments to monitor"""
         return self.client.environments.list(all_tenants=True)
+
+
+class PackageDiscovery(_BaseDiscovery):
+    def discover(self, manager, param=None):
+        """Discover packages to monitor"""
+        package_list = self.client.packages.list(include_disabled=True, limit=1000)
+        packages = list(set([p.fully_qualified_name for p in package_list]))
+        return packages
