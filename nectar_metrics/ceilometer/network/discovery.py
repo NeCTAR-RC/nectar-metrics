6f8d7327411f6b6140a47ad82114bfc84a7f9e57
Jake Yip <jake.yip@unimelb.edu.au>
2021-08-19 22:11:53 +1000

    Refactor NetworkIPAvailabilityPollster
    Move the project filter function from Discovery to Pollster so that more
    Pollsters use this same discovery
    
    Change-Id: I87e9bed4ddefa7fdb910df6e545cd55fe69e9758

--- nectar_metrics/ceilometer/network/discovery.py
+++ nectar_metrics/ceilometer/network/discovery.py
@@ -1,30 +1,13 @@
-from oslo_config import cfg
-
 from ceilometer import neutron_client
 from ceilometer.polling import plugin_base
-
-
-opt_group = cfg.OptGroup(name='network',
-                         title='Options for network')
-
-OPTS = [
-    cfg.StrOpt('ipavailability_project_filter',
-               default=None,
-               help="Only list networks owned by a certain project")
-]
 
 
 class NetworkIPAvailabilityDiscovery(plugin_base.DiscoveryBase):
 
     def __init__(self, conf):
         super(NetworkIPAvailabilityDiscovery, self).__init__(conf)
-        conf.register_group(opt_group)
-        conf.register_opts(OPTS, group=opt_group)
         self.neutron_cli = neutron_client.Client(conf).client
 
     def discover(self, manager, param=None):
         url = '/network-ip-availabilities?ip_version=4'
-        if self.conf.network.ipavailability_project_filter:
-            project = self.conf.network.ipavailability_project_filter
-            url = url + '&tenant_id=%s' % project
         return self.neutron_cli.get(url)['network_ip_availabilities']
--- nectar_metrics/ceilometer/network/pollster.py
+++ nectar_metrics/ceilometer/network/pollster.py
@@ -3,12 +3,26 @@ import itertools
 from ceilometer.polling import plugin_base
 from ceilometer import sample
 
+from oslo_config import cfg
 from oslo_log import log
+
 LOG = log.getLogger(__name__)
 
+opt_group = cfg.OptGroup(name='network',
+                         title='Options for network')
+
+OPTS = [
+    cfg.StrOpt('ipavailability_project_filter',
+               default=None,
+               help="Only list networks owned by a certain project")
+]
 
 class NetworkIPAvailabilityPollster(plugin_base.PollsterBase):
     """Collect stats on network IP availability"""
+    def __init__(self, conf):
+        super(NetworkIPAvailabilityPollster, self).__init__(conf)
+        conf.register_group(opt_group)
+        conf.register_opts(OPTS, group=opt_group)
 
     @property
     def default_discovery(self):
@@ -16,6 +30,9 @@ class NetworkIPAvailabilityPollster(plugin_base.PollsterBase):
 
     def get_samples(self, manager, cache, resources):
         samples = []
+
+        if self.conf.network.ipavailability_project_filter:
+            resources = [r for r in resources if r['tenant_id'] == self.conf.network.ipavailability_project_filter]
 
         for network in resources:
             samples.append(sample.Sample(
